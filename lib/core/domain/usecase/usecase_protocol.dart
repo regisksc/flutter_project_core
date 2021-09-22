6c0808fbbf4cca68f13d112bcfc604abd2e84c4e
Regis Kian <regisksc@gmail.com>
2021-09-22 02:24:08 -0300

    added usecase abstraction

---
+++ lib/core/domain/usecase/usecase_protocol.dart
@@ -0,0 +1,10 @@
+import '../../../core.dart';
+
+abstract class Usecase<Output, Input> {
+  Future<Either<Failure, Output>> call(Input params);
+}
+
+class NoParams extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
